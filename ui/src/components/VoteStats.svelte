4a268b62f1a75aac82921750a454266bed20faf3
Tom Linton <tlinton@fastmail.fm>
2022-02-02 15:22:43 +1300

    Add vote stats

---
+++ ui/src/components/VoteStats.svelte
@@ -0,0 +1,19 @@
+<div>
+  <h3 class="text-lg leading-6 font-medium text-gray-900">Vote Stats</h3>
+  <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+    <div class="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
+      <dt class="text-sm font-medium text-gray-500 truncate">Voting Addresses</dt>
+      <dd class="mt-1 text-3xl font-semibold text-gray-900">71,897</dd>
+    </div>
+
+    <div class="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
+      <dt class="text-sm font-medium text-gray-500 truncate">Proposals</dt>
+      <dd class="mt-1 text-3xl font-semibold text-gray-900">30</dd>
+    </div>
+
+    <div class="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
+      <dt class="text-sm font-medium text-gray-500 truncate">Vote Supply</dt>
+      <dd class="mt-1 text-3xl font-semibold text-gray-900">15,000,000</dd>
+    </div>
+  </dl>
+</div>
