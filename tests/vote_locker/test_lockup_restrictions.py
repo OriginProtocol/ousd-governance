5a2978a892c8e136e72af42ad4bd07117c92c903
Tom Linton <tlinton@fastmail.fm>
2022-03-02 11:09:19 +1300

    Move files

---
+++ tests/vote_locker/test_lockup_restrictions.py
@@ -0,0 +1,7 @@
+import brownie
+from ..fixtures import token, vote_locker
+
+def test_cant_lockup_above_max(vote_locker, token, now):
+    token.approve(vote_locker, 100e18)
+    with brownie.reverts("End must be before maximum lockup time"):
+        vote_locker.upsertLockup(1, now + 86400 * 365 * 5) # 5 years
