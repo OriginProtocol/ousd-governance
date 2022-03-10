e48034d3cb5a275332fd197382f21c167916961c
Tom Linton <tlinton@fastmail.fm>
2022-03-10 14:20:18 +1300

    Structure for listener

---
+++ listener/build/index.js
@@ -0,0 +1,36 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var web3_1 = __importDefault(require("web3"));
+var ethereum_events_1 = __importDefault(require("ethereum-events"));
+var governance_localhost_json_1 = __importDefault(require("../client/networks/governance.localhost.json"));
+var WEB3_PROVIDER = process.env.WEB3_PROVIDER || "http://localhost:8545";
+var contracts = [
+    {
+        name: "Governance",
+        address: governance_localhost_json_1.default.Governance.address,
+        abi: governance_localhost_json_1.default.Governance.abi,
+        events: ["ProposalCreated"],
+    },
+    {
+        name: "VoteLockerCurve",
+        address: governance_localhost_json_1.default.VoteLockerCurve.address,
+        abi: governance_localhost_json_1.default.VoteLockerCurve.abi,
+        events: ["Lockup"],
+    },
+];
+var options = {
+    pollInterval: 13000,
+    confirmations: 12,
+    chunkSize: 10000,
+    concurrency: 10,
+    backoff: 1000, // retry backoff in milliseconds (default: 1000)
+};
+var web3 = new web3_1.default(WEB3_PROVIDER);
+var ethereumEvents = new ethereum_events_1.default(web3, contracts, options);
+ethereumEvents.on("block.confirmed", function (blockNumber, events, done) {
+    console.log(events);
+});
+ethereumEvents.start(startBlock);
---
+++ listener/src/index.ts
@@ -0,0 +1,39 @@
+import Web3 from "web3";
+import EthereumEvents from "ethereum-events";
+import prisma from "../client/lib/prisma.ts";
+import GovernanceContracts from "../client/networks/governance.localhost.json";
+
+const WEB3_PROVIDER = process.env.WEB3_PROVIDER || "http://localhost:8545";
+
+const contracts = [
+  {
+    name: "Governance",
+    address: GovernanceContracts.Governance.address,
+    abi: GovernanceContracts.Governance.abi,
+    events: ["ProposalCreated"],
+  },
+  {
+    name: "VoteLockerCurve",
+    address: GovernanceContracts.VoteLockerCurve.address,
+    abi: GovernanceContracts.VoteLockerCurve.abi,
+    events: ["Lockup"],
+  },
+];
+
+const options = {
+  pollInterval: 13000, // period between polls in milliseconds (default: 13000)
+  confirmations: 12, // n° of confirmation blocks (default: 12)
+  chunkSize: 10000, // n° of blocks to fetch at a time (default: 10000)
+  concurrency: 10, // maximum n° of concurrent web3 requests (default: 10)
+  backoff: 1000, // retry backoff in milliseconds (default: 1000)
+};
+
+const web3 = new Web3(WEB3_PROVIDER);
+
+const ethereumEvents = new EthereumEvents(web3, contracts, options);
+
+ethereumEvents.on("block.confirmed", (blockNumber, events, done) => {
+  console.log(events);
+});
+
+ethereumEvents.start(startBlock);
