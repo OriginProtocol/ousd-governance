24444cc1bea72e3dbc751ac0fcae8210bb29324d
Tom Linton <tlinton@fastmail.fm>
2022-03-14 13:43:13 +1300

    Remove bottom border from section table

--- client/components/SectionTitle.tsx
+++ client/components/SectionTitle.tsx
@@ -1,5 +1,5 @@
 export const SectionTitle = ({ children }) => (
-  <div className="pb-2 mb-5 border-b border-gray-200 mt-12">
+  <div className="pb-2 mb-5 mt-12">
     <h3 className="text-lg leading-6 font-medium text-gray-900">{children}</h3>
   </div>
 );
