df97aa4f421556392786abbe3cc30c7c2826083f
Tom Linton <tlinton@fastmail.fm>
2022-03-16 13:53:32 +1300

    Fix attributes on icon

--- client/components/Disconnected.tsx
+++ client/components/Disconnected.tsx
@@ -7,11 +7,11 @@ export function Disconnected({}) {
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
-        stroke-width="2"
+        strokeWidth="2"
       >
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z"
         />
       </svg>
