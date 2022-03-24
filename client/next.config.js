a8593b3b5af6b0826b173417e2c437ce1c6b6a21
Tom Linton <tlinton@fastmail.fm>
2022-03-24 16:33:07 +1300

    Disable typescript check in build

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Important: return the modified config
     return config;
