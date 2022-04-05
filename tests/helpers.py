4fcd027d6f18f4b667e9602d8fbcd1da84cdc261
Tom Linton <tlinton@fastmail.fm>
2022-04-06 11:16:50 +1200

    Add withdrawal tests

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,3 +1,5 @@
+from brownie import chain
+
 DAY = 86400
 WEEK = 7 * DAY
 
@@ -8,3 +10,11 @@ def approx(a, b, precision=1e-10):
 
 def floor_week(timestamp):
     return timestamp - (timestamp % WEEK)
+
+# Mine `amount` blocks using hardhat_mine, defaults to the length of the governance
+# voting period (45818 blocks ~1 week )
+def mine_blocks(web3, amount="0xB2FA", interval="0x1"):
+    result = web3.provider.make_request("hardhat_mine", [amount, interval])
+    # Using hardhat_mine seems to break the 0 base fee
+    web3.provider.make_request("hardhat_setNextBlockBaseFeePerGas", ["0x0"])
+    chain.mine()
