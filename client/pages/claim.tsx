213723919ad62b316fba1f05dbc6e208ff3cff0a
Tom Hirst <hello@tomhirst.com>
2022-05-05 18:02:29 +0100

    Add claim page template

---
+++ client/pages/claim.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import { PageTitle } from "components/PageTitle";
+
+interface ClaimPageProps {}
+
+const ClaimPage: NextPage<ClaimPageProps> = () => {
+  return (
+    <>
+      <PageTitle>Claim OGV</PageTitle>
+    </>
+  );
+};
+
+export default ClaimPage;
