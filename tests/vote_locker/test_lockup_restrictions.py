bd3fd24712118812e641fef26b9ab7e1d6721647
Tom Linton <tlinton@fastmail.fm>
2022-05-11 12:02:54 +1200

    Prevent zero token lockups (#71)

--- tests/vote_locker/test_lockup_restrictions.py
+++ tests/vote_locker/test_lockup_restrictions.py
@@ -6,3 +6,8 @@ def test_cant_lockup_above_max(vote_locker, token):
     token.approve(vote_locker, 100e18)
     with brownie.reverts("End must be before maximum lockup time"):
         vote_locker.lockup(1, chain.time() + 86400 * 365 * 5) # 5 years
+
+def test_cant_lockup_zero_tokens(vote_locker, token):
+    with brownie.reverts("Amount must be greater than 0"):
+        vote_locker.lockup(0, chain.time() + 86400 * 365)
+
