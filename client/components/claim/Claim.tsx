0d864772a173d9594c4f8d076e6dfa341e02d09a
Tom Hirst <hello@tomhirst.com>
2022-06-02 11:05:40 +0100

    Add automated global lockup alerts

--- client/components/claim/Claim.tsx
+++ client/components/claim/Claim.tsx
@@ -1,5 +1,8 @@
-import { FunctionComponent, useState } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
 import moment from "moment";
+import { toast } from "react-toastify";
+import { sample, random } from "lodash";
+import { truncateEthAddress } from "utils";
 import { SectionTitle } from "components/SectionTitle";
 import Wrapper from "components/Wrapper";
 import Card from "components/Card";
@@ -85,6 +88,46 @@ const Claim: FunctionComponent<ClaimProps> = ({ handlePrevStep }) => {
   const oneYearFromNow = new Date(
     now.getTime() + 1 * 365 * 24 * 60 * 60 * 1000
   );
+
+  useEffect(() => {
+    // @TODO Replace with real lockup data
+    const recentLockups = [
+      {
+        address: "0x57b0dd7967955c92b6e34a038b47fee63e1efd1a",
+        ogvLockedUp: 10000,
+        durationInWeeks: 208,
+      },
+      {
+        address: "0x2C6B8C19dd7174F6e0cc56424210F19EeFe62f94",
+        ogvLockedUp: 5000,
+        durationInWeeks: 156,
+      },
+      {
+        address:
+          "0x3b9bddaccf50023e95592b449d5edf57d6ea483b2cc50a5620cf6cb8dbfb8229",
+        ogvLockedUp: 1234,
+        durationInWeeks: 52,
+      },
+    ];
+
+    setInterval(async () => {
+      const lockup = sample(recentLockups);
+      const { address, ogvLockedUp, durationInWeeks } = lockup;
+      const shortAddress =
+        web3Provider?.chainId === 1
+          ? (await web3Provider.lookupAddress(address)) ||
+            truncateEthAddress(address)
+          : truncateEthAddress(address);
+
+      toast.success(
+        `${shortAddress} just locked up ${ogvLockedUp} OGV for ${durationInWeeks} weeks`,
+        {
+          hideProgressBar: true,
+          position: "bottom-right",
+        }
+      );
+    }, random(5000, 20000, true));
+  }, [web3Provider]);
 
   if (!web3Provider) {
     return (
