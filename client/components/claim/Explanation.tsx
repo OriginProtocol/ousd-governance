f6f86540f447d13840f472a570a94417e255a575
Tom Hirst <hello@tomhirst.com>
2022-06-06 18:38:21 +0100

    Add educational content in single column

--- client/components/claim/Explanation.tsx
+++ client/components/claim/Explanation.tsx
@@ -1,13 +1,9 @@
 import { FunctionComponent, useState } from "react";
-import { SectionTitle } from "components/SectionTitle";
 import Card from "components/Card";
-import SlideTracker from "components/SlideTracker";
-import SlideControls from "components/SlideControls";
 import Button from "components/Button";
 import Ogn from "components/claim/explanation/Ogn";
 import Ousd from "components/claim/explanation/Ousd";
 import Ogv from "components/claim/explanation/Ogv";
-import VeOgv from "components/claim/explanation/VeOgv";
 
 interface ExplanationProps {
   handleNextStep: () => void;
@@ -18,42 +14,15 @@ const Explanation: FunctionComponent<ExplanationProps> = ({
   handleNextStep,
   handlePrevStep,
 }) => {
-  const [currentProduct, setCurrentProduct] = useState(0);
-  const products = ["OGN", "OUSD", "OGV", "veOGV"];
-
-  const handleChangeProduct = (dot: number) => setCurrentProduct(dot);
-  const handleNextSlide = () => setCurrentProduct(currentProduct + 1);
-  const handlePrevSlide = () => setCurrentProduct(currentProduct - 1);
-
-  const canAdvance = currentProduct === products.length - 1;
+  const canAdvance = true;
 
   return (
     <>
       <Card>
-        <SectionTitle>Origin Products</SectionTitle>
-        <p className="text-sm text-gray-600 mb-4">
-          If your wallet is eligible, you&apos;ll be able to claim your
-          OGV/veOGV soon. But first, here&apos;s more about the tokens that
-          drive the protocol and their benefits.
-        </p>
-        <div className="space-y-2">
-          <SlideTracker
-            currentSlide={currentProduct}
-            slides={products}
-            onDotClick={handleChangeProduct}
-          />
-          <Card alt>
-            {currentProduct === 0 && <Ogn />}
-            {currentProduct === 1 && <Ousd />}
-            {currentProduct === 2 && <Ogv />}
-            {currentProduct === 3 && <VeOgv />}
-          </Card>
-          <SlideControls
-            currentSlide={currentProduct}
-            slides={products}
-            handleNextSlide={handleNextSlide}
-            handlePrevSlide={handlePrevSlide}
-          />
+        <div className="space-y-8">
+          <Ousd />
+          <Ogv />
+          <Ogn />
         </div>
       </Card>
       <div className="mt-6 flex">
--- client/components/claim/explanation/Ousd.tsx
+++ client/components/claim/explanation/Ousd.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent } from "react";
 import TokenIcon from "components/TokenIcon";
+import Video from "components/Video";
 
 interface OusdProps {}
 
@@ -7,13 +8,28 @@ const Ousd: FunctionComponent<OusdProps> = () => (
   <div className="space-y-4">
     <div className="flex items-center space-x-1">
       <TokenIcon src="/ousd-coin.svg" alt="OUSD" />
-      <h3 className="text-lg leading-6 font-medium">OUSD</h3>
+      <h3 className="text-lg leading-6 font-medium">Origin Dollar (OUSD)</h3>
     </div>
-    <p className="text-sm text-gray-600">
-      Origin Dollar (OUSD) is the first stablecoin that earns a yield while it's
-      still in your wallet. Access DeFi yields without any of the hassles. No
-      staking. No lockups.
-    </p>
+    <Video />
+    <div className="text-sm lg:text-base text-gray-600 space-y-4">
+      <p>
+        OUSD is a yield-bearing stablecoin that grows in your wallet. It offers
+        the simplest and safest way to earn passively from DeFi. Every unit of
+        OUSD is backed 1:1 by the most trusted collateral in crypto, which means
+        that it can be redeemed anytime. There&apos;s no complicated staking
+        required and you don&apos;t have to trust the Origin team to manage your
+        money. An open-source, fully-audited set of smart contracts governs the
+        deployment of funds across conservative yield farming strategies using
+        Aave, Compound, and Curve/Convex.
+      </p>
+      <p>
+        Today, OUSD is an upgrade over ordinary stablecoins. Tomorrow, it&apos;s
+        intended to be the default payment method for Web3. When you pay with
+        OUSD or send it to a friend, the balance continues to grow when it’s
+        received. Stablecoins are crucial to blockchain commerce, and OUSD is
+        the innovation that will bring DeFi to the global economy.
+      </p>
+    </div>
   </div>
 );
 
