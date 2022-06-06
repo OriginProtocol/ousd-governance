858f0302f4637b4ee1f55584ecfdb243337ebb9c
Tom Hirst <hello@tomhirst.com>
2022-06-06 14:52:47 +0100

    Add countdown and initial content

---
+++ client/components/ExternalLinkIcon.tsx
@@ -0,0 +1,8 @@
+import { FunctionComponent } from "react";
+import Image from "next/image";
+
+const ExternalLinkIcon: FunctionComponent = () => (
+  <Image src="/external-link.svg" height={14} width={14} alt="External link" />
+);
+
+export default ExternalLinkIcon;
