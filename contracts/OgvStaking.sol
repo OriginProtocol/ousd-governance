d0386293736cb04f094f55a1fe249be7710024db
Daniel Von Fange <daniel@leancoder.com>
2022-06-07 10:37:29 -0400

    OZ L-01 Natspec Documentation

--- contracts/OgvStaking.sol
+++ contracts/OgvStaking.sol
@@ -6,6 +6,13 @@ import {ERC20} from "OpenZeppelin/openzeppelin-contracts@4.6.0/contracts/token/E
 import {PRBMathUD60x18} from "paulrberg/prb-math@2.5.0/contracts/PRBMathUD60x18.sol";
 import {RewardsSource} from "./RewardsSource.sol";
 
+/// @title OGV Staking
+/// @author Daniel Von Fange
+/// @notice Provides staking, vote power history, vote delegation, and rewards
+/// distribution.
+/// 
+/// The balance received for staking (and thus the voting power and rewards
+/// distribution) goes up exponentially by the end of the staked period. 
 contract OgvStaking is ERC20Votes {
     // 1. Core Storage
     uint256 public immutable epoch;
@@ -76,6 +83,19 @@ contract OgvStaking is ERC20Votes {
 
     // 2. Staking and Lockup Functions
 
+    /// @notice Stake OGV to an address that may not be the same as the
+    /// sender of the funds. This can be used to give staked funds to someone
+    /// else.
+    ///
+    /// If staking before the start of staking (epoch), then the lockup start
+    /// and end dates are shifted forward so that the lockup starts at the
+    /// epoch.
+    ///
+    /// Any rewards previously earned will be paid out.
+    ///
+    /// @param amount OGV to lockup in the stake
+    /// @param duration in seconds for the stake
+    /// @param to address to receive ownership of the stake
     function stake(
         uint256 amount,
         uint256 duration,
@@ -84,6 +104,17 @@ contract OgvStaking is ERC20Votes {
         _stake(amount, duration, to);
     }
 
+    /// @notice Stake OGV
+    ///
+    /// If staking before the start of staking (epoch), then the lockup start
+    /// and end dates are shifted forward so that the lockup starts at the
+    /// epoch.
+    ///
+    /// Any rewards previously earned will be paid out.
+    /// 
+    /// @notice Stake OGV for myself.
+    /// @param amount OGV to lockup in the stake
+    /// @param duration in seconds for the stake
     function stake(
         uint256 amount,
         uint256 duration
@@ -91,6 +122,10 @@ contract OgvStaking is ERC20Votes {
         _stake(amount, duration, msg.sender);
     }
 
+    /// @dev Internal method used for public staking
+    /// @param amount OGV to lockup in the stake
+    /// @param duration in seconds for the stake
+    /// @param to address to receive ownership of the stake
     function _stake(
         uint256 amount,
         uint256 duration,
@@ -118,6 +153,8 @@ contract OgvStaking is ERC20Votes {
         emit Stake(to, lockups[to].length - 1, amount, end, points);
     }
 
+    /// @notice Collect staked OGV for a lockup and any earned rewards.
+    /// @param lockupId the id of the lockup to unstake
     function unstake(uint256 lockupId) external {
         Lockup memory lockup = lockups[msg.sender][lockupId];
         uint256 amount = lockup.amount;
@@ -132,6 +169,19 @@ contract OgvStaking is ERC20Votes {
         emit Unstake(msg.sender, lockupId, amount, end, points);
     }
 
+    /// @notice Extend a stake lockup for additional points.
+    ///
+    /// The stake end time is computed from the current time + duration, just
+    /// like it is for new stakes. So a new stake for seven days duration and
+    /// an old stake extended with a seven days duration would have the same 
+    /// end.
+    /// 
+    /// If an extend is made before the start of staking, the start time for
+    /// the new stake is shifted forwards to the start of staking, which also
+    /// shifts forward the end date.
+    ///
+    /// @param lockupId the id of the old lockup to extend
+    /// @param duration number of seconds from now to stake for
     function extend(uint256 lockupId, uint256 duration) external {
         // duration checked inside previewPoints
         _collectRewards(msg.sender);
@@ -152,6 +202,13 @@ contract OgvStaking is ERC20Votes {
         emit Stake(msg.sender, lockupId, oldAmount, newEnd, newPoints);
     }
 
+    /// @notice Preview the number of points that would be returned for the
+    /// given amount and duration.
+    ///
+    /// @param amount OGV to be staked
+    /// @param duration number of seconds to stake for
+    /// @return points staking points that would be returned
+    /// @return end staking period end date
     function previewPoints(uint256 amount, uint256 duration)
         public
         view
@@ -168,10 +225,16 @@ contract OgvStaking is ERC20Votes {
 
     // 3. Reward functions
 
+    /// @notice Collect all earned OGV rewards.
     function collectRewards() external {
         _collectRewards(msg.sender);
     }
 
+    /// @notice Shows the amount of OGV a user would receive if they collected
+    /// rewards at this time.
+    ///
+    /// @param user to preview rewards for
+    /// @return OGV rewards amount 
     function previewRewards(address user) external view returns (uint256) {
         uint256 supply = totalSupply();
         if (supply == 0) {
@@ -183,6 +246,18 @@ contract OgvStaking is ERC20Votes {
         return (balanceOf(user) * netRewardsPerShare) / 1e12;
     }
 
+    /// @dev Internal function to handle rewards accounting.
+    /// 
+    /// 1. Collect new rewards for everyone
+    /// 2. Calculate this user's rewards and accounting
+    /// 3. Distribute this user's rewards
+    ///
+    /// This function *must* be called before any user balance changes.
+    ///
+    /// This will always update the user's rewardDebtPerShare to match
+    /// accRewardPerShare, which is essential to the accounting.
+    /// 
+    /// @param user to collect rewards for
     function _collectRewards(address user) internal {
         uint256 supply = totalSupply();
         if (supply > 0) {
