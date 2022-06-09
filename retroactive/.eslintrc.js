987a71a3d8d400fcb22031db8c14eb14b56d6c53
Tom Linton <tlinton@fastmail.fm>
2022-06-10 10:22:33 +1200

    Add eslint-config-prettier

--- retroactive/.eslintrc.js
+++ retroactive/.eslintrc.js
@@ -4,8 +4,8 @@ module.exports = {
         "es2021": true
     },
     "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended"
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
