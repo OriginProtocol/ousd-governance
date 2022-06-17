d0001ee3f2cfbd3f20b7bcea25e87e2adb665e8c
Domen Grabec <grabec@gmail.com>
2022-06-17 15:32:42 +0200

    tabs to spaces

--- contracts/tests/MockOGV.sol
+++ contracts/tests/MockOGV.sol
@@ -6,7 +6,7 @@ import { ERC20 } from "OpenZeppelin/openzeppelin-contracts@4.6.0/contracts/token
 contract MockOgv is ERC20 {
     constructor() ERC20("OGV", "OGV") {}
 
-	function mint(address to, uint256 amount) external {
-		_mint(to, amount);
-	}
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
 }
