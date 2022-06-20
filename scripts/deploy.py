9cbee0ecd4b1096a0335e09c97f7a50cc3cb401b
Domen Grabec <grabec@gmail.com>
2022-06-20 23:32:21 +0200

    added burnable methods

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -5,7 +5,6 @@ from brownie import *
 def main(output_file=None):
     accounts.default = accounts[0]
     # accounts.default = accounts.load("rinkeby_deployer")
-
     token = run("deploy_token")
 
     epoch = 86400  # 1 day
@@ -16,6 +15,11 @@ def main(output_file=None):
     timelock_controller = Timelock.deploy([accounts[0]], [accounts[0]])
 
     governance = Governance.deploy(staking, timelock_controller)
+
+    merkle_mandatory = run("deploy_mandatory_lockup_distributor", "main", (token.address, "0x0000000000000000000000000000000000000000", staking.address, 14171617))
+    merkle_optional = run("deploy_optional_lockup_distributor", "main", (token.address, "0x0000000000000000000000000000000000000000", staking.address, 14171617))
+
+    merkle_mandatory.burnRemainingOGV()
 
     # Make the governor the proposer and executor on timelock
     timelock_controller.grantRole(web3.keccak(text="PROPOSER_ROLE"), governance)
--- scripts/deploy_optional_lockup_distributor.py
+++ scripts/deploy_optional_lockup_distributor.py
@@ -1,5 +1,5 @@
 from brownie import *
 
 
-def main(token_address, staking_address, merkle_root):
-    return OptionalLockupDistributor.deploy(token_address, merkle_root, staking_address)
+def main(token_address, staking_address, merkle_root, end_block):
+    return OptionalLockupDistributor.deploy(token_address, merkle_root, staking_address, end_block)
