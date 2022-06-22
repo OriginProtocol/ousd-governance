807805dbab1f4bceb969670b248207aa0b43cdbc
rolandpo <rolopod@gmail.com>
2022-06-22 16:31:26 +0200

    add footer links

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -6,10 +6,66 @@ const Footer: FunctionComponent = () => (
     <Wrapper>
       <div className="w-full space-y-3 sm:space-y-0 sm:flex items-center justify-between">
         <ul className="flex space-x-5 lg:space-x-7">
-          <li>Jobs</li>
-          <li>Docs</li>
-          <li>Terms</li>
-          <li>Privacy</li>
+          <li>
+            <a
+              className="text-gray-400 hover:text-black"
+              href="https://analytics.ousd.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Analytics
+            </a>
+          </li>
+          <li>
+            <a
+              className="text-gray-400 hover:text-black"
+              href="https://angel.co/company/originprotocol/jobs"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Jobs
+            </a>
+          </li>
+          <li>
+            <a
+              className="text-gray-400 hover:text-black"
+              href="https://docs.ousd.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Docs
+            </a>
+          </li>
+          <li>
+            <a
+              className="text-gray-400 hover:text-black"
+              href="https://originprotocol.com/tos"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Terms
+            </a>
+          </li>
+          <li>
+            <a
+              className="text-gray-400 hover:text-black"
+              href="https://originprotocol.com/privacy"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Privacy
+            </a>
+          </li>
+          <li>
+            <a
+              className="text-gray-400 hover:text-black"
+              href="https://discord.gg/jyxpUSe"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Discord
+            </a>
+          </li>
         </ul>
         <div className="flex space-x-5 lg:space-x-7">
           <p>Created by the team at Origin Protocol</p>
