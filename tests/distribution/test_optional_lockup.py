936de881245d7f06f97d4ff76c5648070ab3377b
Tom Linton <tlinton@fastmail.fm>
2022-06-25 10:01:11 +1200

    Remove extra chain mine

--- tests/distribution/test_optional_lockup.py
+++ tests/distribution/test_optional_lockup.py
@@ -1,7 +1,13 @@
 from brownie import *
 import brownie
 from ..helpers import approx, H, MAXTIME, WEEK
-from ..fixtures import optional_lockup_distributor, token, staking, rewards, show_transfers
+from ..fixtures import (
+    optional_lockup_distributor,
+    token,
+    staking,
+    rewards,
+    show_transfers,
+)
 
 merkle_proof = [
     0xC06E0D1A35007D9401AB64B2EDB9CD0A674EBCCE35ACBF4C93E1193F99DF35D3,
@@ -25,9 +31,8 @@ def test_claim_with_lockup_duration(optional_lockup_distributor, token, staking)
     # Transfer to the distributor contract so it has something to give out
     token.transfer(optional_lockup_distributor.address, amount)
     optional_lockup_distributor.claim(1, amount, merkle_proof, WEEK)
-    chain.sleep(WEEK)
-    chain.mine()
     assert staking.lockups(accounts.default, 0)[0] == amount
+
 
 def test_can_not_claim(optional_lockup_distributor, token, staking):
     amount = 500000000 * 1e18
@@ -37,15 +42,18 @@ def test_can_not_claim(optional_lockup_distributor, token, staking):
     with brownie.reverts("Can no longer claim. Claim period expired"):
         optional_lockup_distributor.claim(1, amount, merkle_proof, WEEK)
 
+
 def test_burn_remaining_amount(optional_lockup_distributor, token, staking):
     amount = 500000000 * 1e18
     # Transfer to the distributor contract so it has something to give out
     token.transfer(optional_lockup_distributor.address, amount)
     before_balance = token.balanceOf(accounts.default)
     chain.sleep(WEEK)
-    chain.mine(100) # end block is set to 100 blocks after current fixture block
+    # end block is set to 100 blocks after current fixture block
+    chain.mine(100)
     optional_lockup_distributor.burnRemainingOGV()
     assert token.balanceOf(optional_lockup_distributor) == 0
+
 
 def test_can_not_burn_remaining_amount(optional_lockup_distributor, token, staking):
     amount = 500000000 * 1e18
@@ -53,9 +61,11 @@ def test_can_not_burn_remaining_amount(optional_lockup_distributor, token, staki
     token.transfer(optional_lockup_distributor.address, amount)
     before_balance = token.balanceOf(accounts.default)
     chain.sleep(WEEK)
-    chain.mine(96) # end block is set to 100 blocks after current fixture block
+    # end block is set to 100 blocks after current fixture block
+    chain.mine(96)
     with brownie.reverts("Can not yet burn the remaining OGV"):
         optional_lockup_distributor.burnRemainingOGV()
+
 
 def test_valid_proof(optional_lockup_distributor, token, staking):
     amount = 500000000 * 1e18
@@ -63,10 +73,14 @@ def test_valid_proof(optional_lockup_distributor, token, staking):
     token.transfer(optional_lockup_distributor.address, amount)
     assert optional_lockup_distributor.isProofValid(1, amount, merkle_proof)
 
+
 def test_invalid_proof(optional_lockup_distributor, token, staking):
     amount = 500000000 * 1e18
     # Transfer to the distributor contract so it has something to lockup
     token.transfer(optional_lockup_distributor.address, amount)
     false_merkle_proof = merkle_proof
-    false_merkle_proof[0] = '0xC06E0D1A35007D9401AB64B2EDB9CD0A674EBCCE35ACBF4C93E1193F99DF35D2'
-    assert not optional_lockup_distributor.isProofValid(1, amount, false_merkle_proof)
+    false_merkle_proof[
+        0
+    ] = "0xC06E0D1A35007D9401AB64B2EDB9CD0A674EBCCE35ACBF4C93E1193F99DF35D2"
+    assert not optional_lockup_distributor.isProofValid(
+        1, amount, false_merkle_proof)
