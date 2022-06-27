b5da6c09e3676881da7999042bec328d50605f8e
Tom Linton <tlinton@fastmail.fm>
2022-06-28 10:08:21 +1200

    Run black

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,16 +1,17 @@
 import json
 from brownie import *
 
-DEV_MODE = True # TODO change this (!!!)
-f = open('./scripts/claims_dev.json' if DEV_MODE else './scripts/claims.json')
-merkle_root = json.load(f)['merkleRoot']
+DEV_MODE = True  # TODO change this (!!!)
+f = open("./scripts/claims_dev.json" if DEV_MODE else "./scripts/claims.json")
+merkle_root = json.load(f)["merkleRoot"]
+
 
 def main(output_file=None):
     accounts.default = accounts[0]
     # accounts.default = accounts.load("rinkeby_deployer")
     token = run("deploy_token")
 
-    epoch = 86400 # 1 day
+    epoch = 86400  # 1 day
     rewards = run("deploy_rewards", "main", (token.address,))
     staking = run("deploy_staking", "main", (token.address, epoch, rewards.address))
 
@@ -18,15 +19,23 @@ def main(output_file=None):
 
     governance = Governance.deploy(staking, timelock_controller)
 
-    mandatoryEndBlock = 15333935 # TODO change this (!!!)
-    optionalEndBlock = 15333935 # TODO change this (!!!)
+    mandatory_end_block = 15333935  # TODO change this (!!!)
+    optional_end_block = 15333935  # TODO change this (!!!)
 
-    merkle_mandatory = run("deploy_mandatory_lockup_distributor", "main", (token.address, merkle_root, staking.address, mandatoryEndBlock))
-    merkle_optional = run("deploy_optional_lockup_distributor", "main", (token.address, merkle_root, staking.address, optionalEndBlock))
+    merkle_mandatory = run(
+        "deploy_mandatory_lockup_distributor",
+        "main",
+        (token.address, merkle_root, staking.address, mandatory_end_block),
+    )
+    merkle_optional = run(
+        "deploy_optional_lockup_distributor",
+        "main",
+        (token.address, merkle_root, staking.address, optional_end_block),
+    )
 
     if DEV_MODE:
-        token.transfer(merkle_mandatory, 50000 * 10e18, {'from': accounts[0]})
-        token.transfer(merkle_optional, 50000 * 10e18, {'from': accounts[0]})
+        token.transfer(merkle_mandatory, 50000 * 10e18, {"from": accounts[0]})
+        token.transfer(merkle_optional, 50000 * 10e18, {"from": accounts[0]})
 
     # Make the governor the proposer and executor on timelock
     timelock_controller.grantRole(web3.keccak(text="PROPOSER_ROLE"), governance)
@@ -41,10 +50,22 @@ def main(output_file=None):
                 address=timelock_controller.address, abi=timelock_controller.abi
             ),
             Governance=dict(address=governance.address, abi=governance.abi),
-            MandatoryDistributor=dict(address=merkle_mandatory.address, abi=merkle_mandatory.abi),
-            OptionalDistributor=dict(address=merkle_optional.address, abi=merkle_optional.abi)
+            MandatoryDistributor=dict(
+                address=merkle_mandatory.address, abi=merkle_mandatory.abi
+            ),
+            OptionalDistributor=dict(
+                address=merkle_optional.address, abi=merkle_optional.abi
+            ),
         )
         with open(output_file, "w+") as f:
             json.dump(output, f, indent=2)
 
-    return (token, staking, timelock_controller, governance, merkle_mandatory, merkle_optional, rewards)
+    return (
+        token,
+        staking,
+        timelock_controller,
+        governance,
+        merkle_mandatory,
+        merkle_optional,
+        rewards,
+    )
--- scripts/deploy_optional_lockup_distributor.py
+++ scripts/deploy_optional_lockup_distributor.py
@@ -2,4 +2,6 @@ from brownie import *
 
 
 def main(token_address, staking_address, merkle_root, end_block):
-    return OptionalLockupDistributor.deploy(token_address, staking_address, merkle_root, end_block)
+    return OptionalLockupDistributor.deploy(
+        token_address, staking_address, merkle_root, end_block
+    )
