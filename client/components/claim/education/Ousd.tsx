b1436297b4dc65954bb17a7d8e896f8f8fa8c587
Tom Hirst <hello@tomhirst.com>
2022-07-04 16:12:46 +0100

    Add text content to educational steps

--- client/components/claim/education/Ousd.tsx
+++ client/components/claim/education/Ousd.tsx
@@ -53,26 +53,46 @@ const Ousd: FunctionComponent<OusdProps> = ({ onComplete }) => (
           </h3>
         </div>
         <Video />
-        <div className="text-sm lg:text-base text-gray-600 space-y-4">
+        <div className="text-sm lg:text-base text-gray-800 space-y-4">
           <p>
-            OUSD is a yield-bearing stablecoin that grows in your wallet. It
-            offers the simplest and safest way to earn passively from DeFi.
-            Every unit of OUSD is backed 1:1 by the most trusted collateral in
-            crypto, which means that it can be redeemed anytime. There&apos;s no
-            complicated staking required and you don&apos;t have to trust the
-            Origin team to manage your money. An open-source, fully-audited set
-            of smart contracts governs the deployment of funds across
-            conservative yield farming strategies using Aave, Compound, and
-            Curve/Convex.
+            <strong>Origin Dollar</strong> — or <strong>OUSD</strong> — is a
+            stablecoin that gives you access to some of the{" "}
+            <strong>best yields in DeFi</strong> with{" "}
+            <strong>none of the hassle</strong>.
           </p>
           <p>
-            Today, OUSD is an upgrade over ordinary stablecoins. Tomorrow,
-            it&apos;s intended to be the default payment method for Web3. When
-            you pay with OUSD or send it to a friend, the balance continues to
-            grow when it’s received. Stablecoins are crucial to blockchain
-            commerce, and OUSD is the innovation that will bring DeFi to the
-            global economy.
+            “<strong>Stablecoins</strong>” offer the{" "}
+            <strong>
+              benefits of cryptocurrencies without the price volatility
+            </strong>
+            .
           </p>
+          <p>
+            What separates OUSD from ordinary stablecoins is that it grows in
+            your wallet every day. Not only does OUSD passively earn interest
+            while you hold it, you can use it to make purchases and transfers
+            anywhere around the world.
+          </p>
+          <p>
+            OUSD&apos;s price is pegged to the dollar and it&apos;s{" "}
+            <strong>backed 1:1 by the most trusted collateral in crypto</strong>
+            , which means it can be redeemed anytime.
+          </p>
+          <p>
+            Simply put:{" "}
+            <strong>imagine your wallet filled with OUSD bills</strong>
+          </p>
+          <ul className="list-outside list-disc space-y-2 ml-4">
+            <li>
+              If you kept that OUSD in your wallet, then your wallet would fill
+              with more OUSD every day.
+            </li>
+            <li>
+              And if you wanted to take OUSD from your wallet to make a purchase
+              or send it to a friend, you can do that too.
+            </li>
+          </ul>
+          <p>It brings you the best of both worlds.</p>
         </div>
       </div>
     </Card>
