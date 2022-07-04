100da490bbc48b3d8d007da90740b84929982aba
Tom Linton <tlinton@fastmail.fm>
2022-07-05 11:17:39 +1200

    Retroactive fixes (#124)
    * Retroactive scaffold for Curve pool
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Remove progress.json
    
    * Fix typo and ignore progress file
    
    * Start handling rebases
    
    * Start adding other sources
    
    * Add more addresses and ABIs
    
    * Remove uniswap
    
    * Cleanup and improvements
    
    * Remove yarn-error.log
    
    * Sample run for OUSD/WOUSD
    
    * Remove unused merge
    
    * Remove debug log
    
    * Cleanup and work on OGN/LM script
    
    * Finish up outputs for OGN
    
    * Add README
    
    * Add announce block
    
    * Update snapshot blocks
    
    * Add generated data up to recent block
    
    * Fix typo
    
    * Cleanup undefined check
    
    * Set OUSD redeploy block
    
    * Comment formatting
    
    * Add separate cumulative and balance rewards scoring functions
    
    * Add OGN staking contract
    
    * Implementation not proxy ABI for OGN staking
    
    * Add OGN staking rewards and prettier
    
    * Regenerate OUSD claim and add some validation
    
    * Remove OGN progress file
    
    * Fixes and add some validation to OGN tree
    
    * Add eslint-config-prettier
    
    * Fix JSON parse on staking ABI
    
    * Add saves when querying complete
    
    * Fix 3crv gauge event values
    
    * Fix formatting
    
    * Fix equivalence check in reward calculation
    
    * Fixes for cumulative reward calculation
    
    * Caching for historical ognStakingRewards queries, and fixes for bn actions
    
    * Regenerate OUSD claims
    
    * Remove debug
    
    * Add commented debug
    
    * Convert to 1e18
    
    * Add excluded contracts to OUSD tree and run prettier
    
    * Move ousd data output path to scripts dir
    
    * Save temporary generated data
    
    * Exclude some contracts
    
    * Regenerate with excluded contracts
    
    * Increase snapshot block
    
    * Filter out OGN staking contract from OGN rewards
    
    * Update data
    
    * Fix output file paths
    
    * Add optional claims file
    
    * Rinkeby deploy
    
    * LFS storage
    
    * Fix amount sum and exclude 0 reward addresses
    
    * Add some debugging statements
    
    * Add staking rewards cache
    
    * Prettier
    
    * Separate OGN staking out, fix rounding errors and bug in summation
    
    * Regenerate data, remove LFS since no longer needed
    
    * Fresh Rinkeby deploy of contracts
    
    * Remove gitattributes

--- retroactive/src/ogn.ts
+++ retroactive/src/ogn.ts
@@ -200,7 +200,7 @@ ethereumEvents.on(
 
       const csv = Object.entries(claims).map(
         ([address, data]: [address: string, data: any]) => {
-          return `${address}, ${["ogn", "ousd3Crv", "ousd3CrvGauge", "convex"]
+          return `${address}, ${["ogn", "ognStaking", "ousd3Crv", "ousd3CrvGauge", "convex"]
             .map((key) =>
               data.split[key] === undefined ? 0 : data.split[key].toString()
             )
@@ -209,9 +209,15 @@ ethereumEvents.on(
       );
 
       // Write a CSV for for easier verification
-      fs.writeFileSync("../scripts/1_data/optional_lockup_rewards.csv", csv.join("\n"));
+      fs.writeFileSync(
+        "../scripts/1_data/optional_lockup_rewards.csv",
+        csv.join("\n")
+      );
       // Write merkle tree claims JSON structure
-      fs.writeFileSync("../scripts/1_data/optional_lockup_accounts.json", JSON.stringify(claims));
+      fs.writeFileSync(
+        "../scripts/1_data/optional_lockup_accounts.json",
+        JSON.stringify(claims)
+      );
     }
 
     done();
@@ -221,7 +227,19 @@ ethereumEvents.on(
 const calculateRewards = async () => {
   console.log("\n");
   console.log("Calculating OGN rewards");
-  const ognRewards = balanceRewardScore(Object.fromEntries(Object.entries(ognHolders).filter(([k]) => k !== ognStakingContract.address)));
+  const ognRewards = balanceRewardScore(
+    Object.fromEntries(
+      Object.entries(ognHolders).filter(
+        ([k]) => k !== ognStakingContract.address
+      )
+    )
+  );
+
+  const ognInstance = new ethers.Contract(
+    ognContract.address,
+    ognContract.abi,
+    provider
+  );
 
   const ognStakingContractInstance = new ethers.Contract(
     ognStakingContract.address,
@@ -256,14 +274,22 @@ const calculateRewards = async () => {
   console.log("Calculating OUSD3CRV-f rewards");
   const ousd3CrvRewards = cumulativeRewardScore(
     // Exclude OUSD3CRV Gauge from rewards as it will have the most
-    Object.fromEntries(Object.entries(ousd3Crv).filter(([k]) => k !== ousd3CrvGaugeContract.address)),
+    Object.fromEntries(
+      Object.entries(ousd3Crv).filter(
+        ([k]) => k !== ousd3CrvGaugeContract.address
+      )
+    ),
     SNAPSHOT_BLOCK,
     ANNOUNCE_BLOCK
   );
   console.log("Calculating OUSD3CRV-f-gauge rewards");
   const ousd3CrvGaugeRewards = cumulativeRewardScore(
     // Exclude Convex finance voter proxy
-    Object.fromEntries(Object.entries(ousd3CrvGauge).filter(([k]) => k !== "0x989AEb4d175e16225E39E87d0D97A3360524AD80")),
+    Object.fromEntries(
+      Object.entries(ousd3CrvGauge).filter(
+        ([k]) => k !== "0x989AEb4d175e16225E39E87d0D97A3360524AD80"
+      )
+    ),
     SNAPSHOT_BLOCK,
     ANNOUNCE_BLOCK
   );
@@ -289,7 +315,13 @@ const calculateRewards = async () => {
     return total.add(amount);
   }, bigNumberify(0));
 
-  const totalOgnScore = totalOgnHoldersScore.add(totalOgnStakersScore);
+  const totalOgnScore = totalOgnHoldersScore.add(
+      // Account for mistaken transfers to the staking contract by using
+      // balanceOf rather than summing all the stakes
+      await ognInstance.balanceOf(ognStakingContract.address, {
+        blockTag: SNAPSHOT_BLOCK,
+      })
+  );
 
   const totalOusd3CrvRewardsScore = Object.values(
     ousd3CrvRewards
@@ -318,6 +350,7 @@ const calculateRewards = async () => {
   const addresses = [
     ...new Set(
       Object.keys(ognRewards)
+        .concat(Object.keys(ognStakingRewards))
         .concat(Object.keys(ousd3CrvRewards))
         .concat(Object.keys(ousd3CrvGaugeRewards))
         .concat(Object.keys(convexRewards))
@@ -355,19 +388,24 @@ const calculateRewards = async () => {
           .div(totalLiquidityMiningScore)
       : bigNumberify(0);
 
-    acc[address] = {
-      amount: ognReward
-        .add(ousd3CrvReward)
-        .add(ousd3CrvGaugeReward)
-        .add(convexReward),
-      split: {
-        ogn: ognReward,
-        ognStaking: ognStakingReward,
-        ousd3Crv: ousd3CrvReward,
-        ousd3CrvGauge: ousd3CrvGaugeReward,
-        convex: convexReward,
-      },
-    };
+    const amount = ognReward
+      .add(ognStakingReward)
+      .add(ousd3CrvReward)
+      .add(ousd3CrvGaugeReward)
+      .add(convexReward);
+
+    if (amount.gt(0)) {
+      acc[address] = {
+        amount,
+        split: {
+          ogn: ognReward,
+          ognStaking: ognStakingReward,
+          ousd3Crv: ousd3CrvReward,
+          ousd3CrvGauge: ousd3CrvGaugeReward,
+          convex: convexReward,
+        },
+      };
+    }
     return acc;
   }, {});
 
@@ -398,6 +436,18 @@ const calculateRewards = async () => {
         .add(account.split.convex);
     },
     bigNumberify(0)
+  );
+
+  console.log(`Total OGN score`, totalOgnScore.toString());
+  console.log("Total OGN excluding staked", totalOgnHoldersScore.toString());
+  console.log("Total OGN staked (sum)", totalOgnStakersScore.toString());
+  console.log(
+    "Total OGN staked (balanceOf)",
+    (
+      await ognInstance.balanceOf(ognStakingContract.address, {
+        blockTag: SNAPSHOT_BLOCK,
+      })
+    ).toString()
   );
 
   console.log(
