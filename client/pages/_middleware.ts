fd363792c925592037749f073cb2293ad97c7877
Domen Grabec <grabec@gmail.com>
2022-07-05 18:16:32 +0200

    add api calls as redirect exceptions

--- client/pages/_middleware.ts
+++ client/pages/_middleware.ts
@@ -4,10 +4,11 @@ import { claimIsOpen } from "utils";
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
   const regEx = /\.(.*)$/;
-  const hasExtension = regEx.test(pathname);
+  const apiRegEx = /^\/api\/.*/;
+  const redirectException = regEx.test(pathname) || apiRegEx.test(pathname);
 
   // If the claim hasn't started
-  if (!hasExtension && pathname !== "/claim" && claimIsOpen()) {
+  if (!redirectException && pathname !== "/claim" && claimIsOpen()) {
     // Redirect everything to /claim
     const claimUrl = req.nextUrl.clone();
     claimUrl.pathname = "/claim";
