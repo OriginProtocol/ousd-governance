0bd6e5dded2bc00f8d97823f2fcb8421c617adc8
Tom Hirst <hello@tomhirst.com>
2022-07-05 14:14:51 +0100

    Change sticky sidebar package

--- client/components/claim/Education.tsx
+++ client/components/claim/Education.tsx
@@ -1,12 +1,11 @@
 import { FunctionComponent, useState } from "react";
-import { StickyContainer, Sticky } from "react-sticky";
 import classNames from "classnames";
-import CardGroup from "components/CardGroup";
 import Card from "components/Card";
 import Button from "components/Button";
 import Ogn from "@/components/claim/education/Ogn";
 import Ousd from "@/components/claim/education/Ousd";
 import Ogv from "@/components/claim/education/Ogv";
+import Sticky from "react-sticky-el";
 
 interface EducationProps {
   handleNextStep: () => void;
@@ -29,44 +28,40 @@ const Education: FunctionComponent<EducationProps> = ({
 
   return (
     <div className="lg:flex">
-      <StickyContainer>
-        <Sticky>
-          {({ style }) => (
-            <div style={style}>
-              <div className="lg:w-40 lg:-ml-44 xl:w-56 xl:-ml-60 lg:z-10 lg:pt-4">
-                <Card tightPadding alt>
-                  <ul className="space-y-2 md:space-y-4">
-                    {educationSteps.map((step, index) => {
-                      const isCurrent = index === currentEducationStep;
+      <Sticky disabled={window.innerWidth < 768}>
+        <div>
+          <div className="lg:w-40 lg:-ml-44 xl:w-56 xl:-ml-60 lg:z-10 lg:pt-4">
+            <Card tightPadding alt>
+              <ul className="space-y-2 md:space-y-4">
+                {educationSteps.map((step, index) => {
+                  const isCurrent = index === currentEducationStep;
 
-                      const markerClasses = classNames(
-                        "text-sm md:text-base rounded h-6 w-6 flex flex-shrink-0 items-center justify-center",
-                        {
-                          "bg-primary text-white": isCurrent,
-                          "bg-gray-300 text-gray-400": !isCurrent,
-                        }
-                      );
-                      const textClasses = classNames("text-sm md:text-base", {
-                        "text-gray-500": !isCurrent,
-                        "text-black": isCurrent,
-                      });
-                      return (
-                        <li
-                          key={step}
-                          className="flex items-center space-x-2 lg:space-x-3"
-                        >
-                          <span className={markerClasses}>{index + 1}</span>
-                          <span className={textClasses}>{step}</span>
-                        </li>
-                      );
-                    })}
-                  </ul>
-                </Card>
-              </div>
-            </div>
-          )}
-        </Sticky>
-      </StickyContainer>
+                  const markerClasses = classNames(
+                    "text-sm md:text-base rounded h-6 w-6 flex flex-shrink-0 items-center justify-center",
+                    {
+                      "bg-primary text-white": isCurrent,
+                      "bg-gray-300 text-gray-400": !isCurrent,
+                    }
+                  );
+                  const textClasses = classNames("text-sm md:text-base", {
+                    "text-gray-500": !isCurrent,
+                    "text-black": isCurrent,
+                  });
+                  return (
+                    <li
+                      key={step}
+                      className="flex items-center space-x-2 lg:space-x-3"
+                    >
+                      <span className={markerClasses}>{index + 1}</span>
+                      <span className={textClasses}>{step}</span>
+                    </li>
+                  );
+                })}
+              </ul>
+            </Card>
+          </div>
+        </div>
+      </Sticky>
       <div className="mt-4 lg:mt-0">
         {currentEducationStep === 0 && (
           <Ousd onComplete={handleNextEducationStep} />
