73fb4815842d941e6a46e8057bcc7dc6290d17bc
Tom Linton <tlinton@fastmail.fm>
2022-07-05 20:09:43 +1200

    Fix tests

--- tests/governance/test_initial_state.py
+++ tests/governance/test_initial_state.py
@@ -16,7 +16,7 @@ def test_voting_delay(governance):
 
 
 def test_voting_period(governance):
-    assert governance.votingPeriod() == 45818  # 1 week in blocks
+    assert governance.votingPeriod() == 32727  # 1 week in blocks
 
 
 def test_quorum(governance, web3):
