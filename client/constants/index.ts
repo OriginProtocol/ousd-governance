73e0ad0a198af55e428b869a0cd726cf1807bea2
Franck Chastagnol <franck@originprotocol.com>
2022-07-06 20:55:56 -0700

    Fix missing imports

--- client/constants/index.ts
+++ client/constants/index.ts
@@ -2,9 +2,12 @@ import LocalGovernanceContracts from "../networks/governance.localhost.json";
 import RinkebyGovernanceContracts from "../networks/governance.rinkeby.json";
 import MainnetGovernanceContracts from "../networks/governance.mainnet.json";
 
+export const mainnetNetworkUrl = process.env.WEB3_PROVIDER;
+export const rinkebyNetworkUrl = process.env.WEB3_PROVIDER;
+
 export const RPC_URLS = {
-  1: process.env.WEB3_PROVIDER,
-  4: process.env.WEB3_PROVIDER,
+  1: mainnetNetworkUrl,
+  4: rinkebyNetworkUrl,
   31337: "http://localhost:8545",
 };
 
