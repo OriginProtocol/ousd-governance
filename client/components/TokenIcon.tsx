59601f9111f7d9bcd46f3a6e99c60ac9de5c4110
Tom Hirst <hello@tomhirst.com>
2022-07-22 14:30:12 +0100

    Pull acrrued rewards figure from staking contract (#256)
    * Pull acrrued rewards figure from staking contract
    
    * Make account balances 4 column
    
    * Add collect rewards button if user has rewards to collect
    
    * Stack buttons on mobile
    
    * Add call out message for % staked and % vAPY
    
    * Change OGV availability text + add logos

--- client/components/TokenIcon.tsx
+++ client/components/TokenIcon.tsx
@@ -5,15 +5,21 @@ interface TokenIconProps {
   src: string;
   alt?: string;
   large?: Boolean;
+  small?: Boolean;
 }
 
-const TokenIcon: FunctionComponent<TokenIconProps> = ({ src, alt, large }) => (
+const TokenIcon: FunctionComponent<TokenIconProps> = ({
+  src,
+  alt,
+  large,
+  small,
+}) => (
   <div className="flex items-center flex-shrink-0">
     <Image
       src={src}
       alt={alt}
-      height={large ? 38 : 28}
-      width={large ? 38 : 28}
+      height={large ? 38 : small ? 24 : 28}
+      width={large ? 38 : small ? 24 : 28}
     />
   </div>
 );
