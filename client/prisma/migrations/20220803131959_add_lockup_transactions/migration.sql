d9d024996ceca87416d9ea9447e650b33ac1c4be
Tom Hirst <hello@tomhirst.com>
2022-08-03 14:20:40 +0100

    Try to fix db migrations

--- client/prisma/migrations/20220803131959_add_lockup_transactions/migration.sql
+++ client/prisma/migrations/20220803131959_add_lockup_transactions/migration.sql
@@ -2,16 +2,12 @@
   Warnings:
 
   - You are about to drop the column `lockupId` on the `lockups` table. All the data in the column will be lost.
-  - You are about to drop the `transaction` table. If the table is not empty, all the data it contains will be lost.
   - A unique constraint covering the columns `[last_seen_block]` on the table `listener` will be added. If there are existing duplicate values, this will fail.
   - A unique constraint covering the columns `[lockup_id,user]` on the table `lockups` will be added. If there are existing duplicate values, this will fail.
   - A unique constraint covering the columns `[proposal_id]` on the table `proposals` will be added. If there are existing duplicate values, this will fail.
   - A unique constraint covering the columns `[address]` on the table `voters` will be added. If there are existing duplicate values, this will fail.
 
 */
--- DropForeignKey
-ALTER TABLE "transaction" DROP CONSTRAINT "transaction_lockupId_fkey";
-
 -- DropIndex
 DROP INDEX "listener_last_seen_block_key";
 
@@ -26,10 +22,8 @@ DROP INDEX "voters_address_key";
 
 -- AlterTable
 ALTER TABLE "lockups" DROP COLUMN "lockupId",
+ADD COLUMN     "active" BOOLEAN NOT NULL DEFAULT true,
 ADD COLUMN     "lockup_id" INTEGER NOT NULL DEFAULT 0;
-
--- DropTable
-DROP TABLE "transaction";
 
 -- CreateTable
 CREATE TABLE "transactions" (
