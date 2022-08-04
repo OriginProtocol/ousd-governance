e8ed3efa3795f1404cb5587b82a456af83dd4d22
Tom Hirst <hello@tomhirst.com>
2022-08-04 12:47:08 +0100

    Remove render block if no wallet is connected

--- client/pages/stake/index.tsx
+++ client/pages/stake/index.tsx
@@ -3,21 +3,9 @@ import CardGroup from "components/CardGroup";
 import Wrapper from "components/Wrapper";
 import AccountBalances from "components/vote-escrow/AccountBalances";
 import YourLockups from "components/vote-escrow/YourLockups";
-import { Disconnected } from "components/Disconnected";
-import { useStore } from "utils/store";
 import Seo from "components/Seo";
 
 export default function VoteEscrow() {
-  const { web3Provider } = useStore();
-
-  if (!web3Provider) {
-    return (
-      <Wrapper narrow>
-        <Disconnected />
-      </Wrapper>
-    );
-  }
-
   return (
     <Wrapper narrow>
       <Seo title="Stake OGV" />
