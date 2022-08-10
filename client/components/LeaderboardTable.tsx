5716342cc3a2357803cf7c7050e3b20e9f9e5970
Tom Hirst <hello@tomhirst.com>
2022-08-10 16:59:15 +0100

    Add more info to voters leaderboard table

--- client/components/LeaderboardTable.tsx
+++ client/components/LeaderboardTable.tsx
@@ -1,7 +1,12 @@
 import { ethers } from "ethers";
-import { Address } from "components/Address";
+import TokenAmount from "components/TokenAmount";
+import { truncateEthAddress } from "utils";
+import { useStore } from "utils/store";
 
 export const LeaderboardTable = ({ voters }: { voters: Array }) => {
+  const { totalBalances } = useStore();
+  const { totalSupplyVeOgv } = totalBalances;
+
   if (voters.length < 1) {
     return <p className="text-gray-500 text-sm">No voters yet.</p>;
   }
@@ -10,21 +15,27 @@ export const LeaderboardTable = ({ voters }: { voters: Array }) => {
     <table className="table w-full">
       <thead>
         <tr>
-          <th>#</th>
+          <th>Rank</th>
           <th>Address</th>
           <th>Votes</th>
+          <th>Vote Weight</th>
+          <th>Proposals Voted</th>
         </tr>
       </thead>
       <tbody>
         {voters.map((voter, index) => (
           <tr key={index}>
             <td>{index + 1}</td>
+            <td>{truncateEthAddress(voter.address)}</td>
             <td>
-              <Address address={voter.address} />
+              <TokenAmount
+                amount={ethers.utils.formatUnits(
+                  ethers.BigNumber.from(voter.votes)
+                )}
+              />
             </td>
-            <td>
-              {ethers.utils.formatUnits(ethers.BigNumber.from(voter.votes))}
-            </td>
+            <td>{(voter.votes / totalSupplyVeOgv) * 100}%</td>
+            <td></td>
           </tr>
         ))}
       </tbody>
