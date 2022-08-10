4be82e7e86b9fc806bc82c50664b3b1479bab9a0
Tom Hirst <hello@tomhirst.com>
2022-08-10 15:31:33 +0100

    Add veOGV icons to vote stats + reword labels

--- client/components/proposal/ProposalVoteStats.tsx
+++ client/components/proposal/ProposalVoteStats.tsx
@@ -1,10 +1,9 @@
-import { ethers } from "ethers";
-import { truncateBalance } from "utils/index";
 import CardGroup from "components/CardGroup";
 import Card from "components/Card";
 import CardLabel from "components/CardLabel";
 import CardStat from "components/CardStat";
 import TokenAmount from "components/TokenAmount";
+import TokenIcon from "components/TokenIcon";
 
 export const ProposalVoteStats = ({
   proposal,
@@ -14,65 +13,77 @@ export const ProposalVoteStats = ({
 }) => {
   return (
     <CardGroup horizontal fourCol>
-      <div>
-        <Card dark tightPadding>
-          <CardLabel>Your Vote Power</CardLabel>
-          <CardStat>
-            <TokenAmount amount={votePower} />
-          </CardStat>
-        </Card>
-      </div>
-      <div>
-        <Card dark tightPadding>
-          <CardLabel>For</CardLabel>
-          <CardStat>
-            <TokenAmount amount={proposal.forVotes} />
-          </CardStat>
-          {!hasVoted && (
-            <button
-              className="btn btn-sm btn-success w-full mt-4"
-              disabled={!votePower.gt(0)}
-              onClick={() => onVote(1)}
-            >
-              Vote
-            </button>
-          )}
-        </Card>
-      </div>
-      <div>
-        <Card dark tightPadding>
-          <CardLabel>Against</CardLabel>
-          <CardStat>
-            <TokenAmount amount={proposal.againstVotes} />
-          </CardStat>
-          {!hasVoted && (
-            <button
-              className="btn btn-sm btn-error w-full mt-4"
-              disabled={!votePower.gt(0)}
-              onClick={() => onVote(0)}
-            >
-              Vote
-            </button>
-          )}
-        </Card>
-      </div>
-      <div>
-        <Card dark tightPadding>
-          <CardLabel>Abstain</CardLabel>
-          <CardStat>
-            <TokenAmount amount={proposal.abstainVotes} />
-          </CardStat>
-          {!hasVoted && (
-            <button
-              className="btn btn-sm w-full mt-4"
-              disabled={!votePower.gt(0)}
-              onClick={() => onVote(2)}
-            >
-              Vote
-            </button>
-          )}
-        </Card>
-      </div>
+      <Card dark tightPadding>
+        <div className="space-y-1">
+          <CardLabel>Your Votes</CardLabel>
+          <div className="flex space-x-1 items-center">
+            <TokenIcon src="/veogv.svg" alt="veOGV" small />
+            <CardStat small>
+              <TokenAmount amount={votePower} />
+            </CardStat>
+          </div>
+        </div>
+      </Card>
+      <Card dark tightPadding>
+        <div className="space-y-1">
+          <CardLabel>For Votes</CardLabel>
+          <div className="flex space-x-1 items-center">
+            <TokenIcon src="/veogv.svg" alt="veOGV" small />
+            <CardStat small>
+              <TokenAmount amount={proposal.forVotes} />
+            </CardStat>
+          </div>
+        </div>
+        {!hasVoted && (
+          <button
+            className="btn btn-sm btn-success w-full mt-4"
+            disabled={!votePower.gt(0)}
+            onClick={() => onVote(1)}
+          >
+            Vote For
+          </button>
+        )}
+      </Card>
+      <Card dark tightPadding>
+        <div className="space-y-1">
+          <CardLabel>Against Votes</CardLabel>
+          <div className="flex space-x-1 items-center">
+            <TokenIcon src="/veogv.svg" alt="veOGV" small />
+            <CardStat small>
+              <TokenAmount amount={proposal.againstVotes} />
+            </CardStat>
+          </div>
+        </div>
+        {!hasVoted && (
+          <button
+            className="btn btn-sm btn-error w-full mt-4"
+            disabled={!votePower.gt(0)}
+            onClick={() => onVote(0)}
+          >
+            Vote Against
+          </button>
+        )}
+      </Card>
+      <Card dark tightPadding>
+        <div className="space-y-1">
+          <CardLabel>Abstain Votes</CardLabel>
+          <div className="flex space-x-1 items-center">
+            <TokenIcon src="/veogv.svg" alt="veOGV" small />
+            <CardStat small>
+              <TokenAmount amount={proposal.abstainVotes} />
+            </CardStat>
+          </div>
+        </div>
+        {!hasVoted && (
+          <button
+            className="btn btn-sm w-full mt-4"
+            disabled={!votePower.gt(0)}
+            onClick={() => onVote(2)}
+          >
+            Abstain
+          </button>
+        )}
+      </Card>
     </CardGroup>
   );
 };
