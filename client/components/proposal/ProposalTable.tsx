1c807f05b7d3b6bd2b0d2c5e75b68263cbc53ca9
Tom Hirst <hello@tomhirst.com>
2022-08-17 11:38:40 +0100

    Remove create proposal button from 0 proposal state

--- client/components/proposal/ProposalTable.tsx
+++ client/components/proposal/ProposalTable.tsx
@@ -41,28 +41,6 @@ const ProposalTable: FunctionComponent<ProposalTableProps> = ({
         <h3 className="mt-2 text-sm font-medium text-gray-900">
           No proposals have been created
         </h3>
-        {web3Provider && (
-          <div className="mt-6">
-            <Link href="/proposals/new" passHref>
-              <a className="btn btn-primary btn-sm">
-                <svg
-                  className="-ml-1 mr-2 h-5 w-5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-                  />
-                </svg>
-                Create Proposal
-              </a>
-            </Link>
-          </div>
-        )}
       </div>
     );
   }
