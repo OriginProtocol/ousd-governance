b33c70d9836d7cb044ee58488bcb15fb85829e5b
Daniel Von Fange <daniel@leancoder.com>
2022-09-13 10:45:17 -0400

    Deploy 01 - gov settings changes

---
+++ scripts/deploy_001_upgrade_governor.py
@@ -0,0 +1,33 @@
+import os
+from brownie import *
+from brownie.network import priority_fee
+
+
+
+def main():
+
+deployer = accounts.add(os.environ["KBS"])
+STAKING = "0x0c4576ca1c365868e162554af8e385dc3e7c66d9"
+FROM = {'from': deployer}
+priority_fee("2 gwei")
+
+# Deploy Timelock
+# timelock = Timelock.deploy([], [], FROM)
+timelock = Timelock.at("0x35918cDE7233F2dD33fA41ae3Cb6aE0e42E0e69F")
+Timelock.publish_source(timelock)
+
+# Deploy Governor
+governance = Governance.deploy(STAKING, timelock, FROM)
+Governance.publish_source(governance)
+
+# Configure Timelock
+
+# Governance can control timelock actions
+timelock.grantRole(web3.keccak(text="PROPOSER_ROLE"), governance, FROM)
+timelock.grantRole(web3.keccak(text="CANCELLER_ROLE"), governance, FROM)
+timelock.grantRole(web3.keccak(text="EXECUTOR_ROLE"), governance, FROM)
+
+# Renounce our deployer powers over the timelock
+timelock.renounceRole(web3.keccak(text="TIMELOCK_ADMIN_ROLE"), accounts[0], FROM)
+
+
