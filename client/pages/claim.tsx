9ad3a4ff54ac3d72792db0de553c7f0517ebcf17
rolandpo <rolopod@gmail.com>
2023-01-16 14:33:09 +0100

    add claim content

--- client/pages/claim.tsx
+++ client/pages/claim.tsx
@@ -9,67 +9,94 @@ import Claim from "components/claim/Claim";
 import { useStore } from "utils/store";
 import { claimOpenTimestampPassed, claimIsOpen } from "utils";
 import Seo from "components/Seo";
+import { PageTitle } from "components/PageTitle";
+import CardGroup from "components/CardGroup";
+import AccountBalances from "components/vote-escrow/AccountBalances";
+import YourLockups from "components/vote-escrow/YourLockups";
+import Card from "components/Card";
+import Button from "components/Button";
+import useClaim from "utils/useClaim";
+import { getRewardsApy } from "utils/apy";
+import Link from "components/Link";
 
 interface ClaimPageProps {}
 
 const ClaimPage: NextPage<ClaimPageProps> = () => {
-  const { claim } = useStore();
-  const { currentStep } = claim;
-
-  const updateCurrentStep = (stepChange) => {
-    useStore.setState({
-      claim: {
-        ...claim,
-        currentStep: currentStep + stepChange,
-      },
-    });
-  };
-  const steps = ["Check Eligibility", "Learn about Origin", "Claim Airdrop"];
-
-  const handleNextStep = () => updateCurrentStep(+1);
-  const handlePrevStep = () => updateCurrentStep(-1);
-
-  const [claimOpenTsPassed, setClaimOpenTsPassed] = useState(
-    claimOpenTimestampPassed()
+  const claim = useClaim();
+  const totalSupplyVeOgv = claim.staking.totalSupplyVeOgvAdjusted || 0;
+  // Standard APY figure, assumes 100 OGV locked for max duration
+  const stakingApy = getRewardsApy(
+    100 * 1.8 ** (48 / 12),
+    100,
+    totalSupplyVeOgv
   );
-  const [claimOpen, setClaimOpen] = useState(claimIsOpen());
-
-  useEffect(() => {
-    // check every second so component re-renders when counter reaches 0
-    const claimOpensTimer = setInterval(() => {
-      setClaimOpenTsPassed(claimOpenTimestampPassed());
-      setClaimOpen(claimIsOpen());
-    }, 1000);
-
-    return () => {
-      clearInterval(claimOpensTimer);
-    };
-  }, []);
 
   return (
-    <div className="space-y-6">
+    <Wrapper narrow>
       <Seo title="Claim OGV" />
-      {!claimOpenTsPassed && (
-        <Wrapper narrow>
-          <HoldingPage />
-        </Wrapper>
-      )}
-      {claimOpen && (
-        <>
-          <Wrapper narrow>
-            <StepTracker currentStep={currentStep} steps={steps} />
-          </Wrapper>
-          <Wrapper narrow>
-            {currentStep == 0 && (
-              <Eligibility handleNextStep={handleNextStep} />
-            )}
-            {currentStep == 1 && <Education handleNextStep={handleNextStep} />}
-            {currentStep == 2 && <Claim handlePrevStep={handlePrevStep} />}
-          </Wrapper>
-        </>
-      )}
-    </div>
+      <PageTitle>Claim</PageTitle>
+      <Card margin>
+        <div className="bg-accent text-white -mt-10 -mx-6 p-10 md:-mx-10 text-center">
+          <h2 className="text-[1.75rem] line-height-[2.125rem]">
+            The claim period has now expired
+          </h2>
+          <div className='mt-3 text-sm font-bold'>
+            {`All unclaimed OGV was burned after 90 days. You can still buy OGV and stake it for up to four years. Current OGV stakers are earning a vAPY of ${stakingApy.toFixed(2)}%.`}
+          </div>
+          <div className='mt-6'>
+            <Link
+              className={"w-[15.5rem] btn rounded-full normal-case space-x-2 btn-lg h-[3.25rem] min-h-[3.25rem] bg-white border-none"}
+              href="/stake"
+            >
+              {"Create a new stake"}
+            </Link>
+          </div>
+        </div>
+        <div className='-mb-10 -mx-6 p-10 md:-mx-10 text-center'>
+          <div>
+            <Link
+              className={"w-[15.5rem] btn rounded-full normal-case space-x-2 btn-lg h-[3.25rem] min-h-[3.25rem] btn-primary border-none"}
+              href="https://blog.originprotocol.com/tokenomics-retroactive-rewards-and-prelaunch-liquidity-mining-campaign-for-ogv-1b20b8ab41c8"
+              type="external"
+              newWindow
+            >
+              {"Read about the airdrop"}
+            </Link>
+          </div>
+          <div className='mt-2'>
+            <Link
+              className={"w-[15.5rem] btn rounded-full normal-case space-x-2 btn-lg h-[3.25rem] min-h-[3.25rem] btn-primary border-none"}
+              href="https://www.ousd.com/burn"
+              type="external"
+              newWindow
+            >
+              {"See the OGV burn"}
+            </Link>
+          </div>
+        </div>
+      </Card>
+    </Wrapper>
   );
 };
 
 export default ClaimPage;
+
+/*<div className="mb-20">
+        <div className="space-y-4 bg-accent text-white -my-10 -mx-6 p-10 md:-mx-10">
+          <h2 className="text-2xl space-y-1">
+            <span className="block border-b pb-3 border-secondary/[.15]">
+              <span className="font-bold bg-secondary px-[4px] py-[1px] rounded">
+                {totalPercentageOfLockedUpOgv.toFixed(2)}%
+              </span>{" "}
+              of all OGV is currently staked
+            </span>
+            <span className="block pt-2">
+              OGV stakers earn{" "}
+              <span className="font-bold bg-secondary px-[4px] py-[1px] rounded">
+                {stakingApy.toFixed(2)}%
+              </span>{" "}
+              variable APY
+            </span>
+          </h2>
+        </div>
+      </div>*/
