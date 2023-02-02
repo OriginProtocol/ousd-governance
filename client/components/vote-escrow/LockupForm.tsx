61987f6e5d2fce1d2ee562e24e285a22d2110216
Hrik Bhowal <hrik.bhowal@gmail.com>
2023-02-01 21:00:13 -0800

    fixed veOgv bug

--- client/components/vote-escrow/LockupForm.tsx
+++ client/components/vote-escrow/LockupForm.tsx
@@ -145,8 +145,15 @@ const LockupForm: FunctionComponent<LockupFormProps> = ({ existingLockup }) => {
   // as specified here: https://github.com/OriginProtocol/ousd-governance/blob/master/contracts/OgvStaking.sol#L21
   const votingDecayFactor = 1.8;
 
-  const veOgvFromOgvLockup =
-    lockupAmount * votingDecayFactor ** (lockupDuration / 12);
+  const epoch = 1657584000;
+  const duration = lockupDuration * SECONDS_IN_A_MONTH;
+  // Since we'll be using blockTimestamp from CURRENT block, calculation will be
+  // a hair outdated... but it's negligible
+  const start = blockTimestamp > epoch ? blockTimestamp : epoch; // In prod, should always be blockTimestamp
+  const end = start + duration;
+  const dist = end - epoch; // Distance between end of staking period and the very beginning when staking was launched
+  const multiplier = dist / 365 / 86400;
+  const veOgvFromOgvLockup = lockupAmount * votingDecayFactor ** multiplier;
 
   const ogvLockupRewardApy = getRewardsApy(
     veOgvFromOgvLockup,
