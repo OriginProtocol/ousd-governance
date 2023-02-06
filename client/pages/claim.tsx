e2dc0c4be9e0ce068433469a3007825fd8e3c75d
Shahul Hameed <shahthepro@gmail.com>
2023-02-06 22:49:43 +0530

    Prettify

--- client/pages/claim.tsx
+++ client/pages/claim.tsx
@@ -40,22 +40,28 @@ const ClaimPage: NextPage<ClaimPageProps> = () => {
           <h2 className="text-[1.75rem] line-height-[2.125rem]">
             The claim period has now expired
           </h2>
-          <div className='mt-3 text-sm font-bold'>
-            {`All unclaimed OGV was burned after 90 days. You can still buy OGV and stake it for up to four years. Current OGV stakers are earning a vAPY of ${stakingApy.toFixed(2)}%.`}
+          <div className="mt-3 text-sm font-bold">
+            {`All unclaimed OGV was burned after 90 days. You can still buy OGV and stake it for up to four years. Current OGV stakers are earning a vAPY of ${stakingApy.toFixed(
+              2
+            )}%.`}
           </div>
-          <div className='mt-6'>
+          <div className="mt-6">
             <Link
-              className={"w-[15.5rem] btn rounded-full normal-case space-x-2 btn-lg h-[3.25rem] min-h-[3.25rem] bg-white border-none"}
+              className={
+                "w-[15.5rem] btn rounded-full normal-case space-x-2 btn-lg h-[3.25rem] min-h-[3.25rem] bg-white border-none"
+              }
               href="/stake"
             >
               {"Create a new stake"}
             </Link>
           </div>
         </div>
-        <div className='-mb-10 -mx-6 p-10 md:-mx-10 text-center'>
+        <div className="-mb-10 -mx-6 p-10 md:-mx-10 text-center">
           <div>
             <Link
-              className={"w-[15.5rem] btn rounded-full normal-case space-x-2 btn-lg h-[3.25rem] min-h-[3.25rem] btn-primary border-none"}
+              className={
+                "w-[15.5rem] btn rounded-full normal-case space-x-2 btn-lg h-[3.25rem] min-h-[3.25rem] btn-primary border-none"
+              }
               href="https://blog.originprotocol.com/tokenomics-retroactive-rewards-and-prelaunch-liquidity-mining-campaign-for-ogv-1b20b8ab41c8"
               type="external"
               newWindow
@@ -63,9 +69,11 @@ const ClaimPage: NextPage<ClaimPageProps> = () => {
               {"Read about the airdrop"}
             </Link>
           </div>
-          <div className='mt-2'>
+          <div className="mt-2">
             <Link
-              className={"w-[15.5rem] btn rounded-full normal-case space-x-2 btn-lg h-[3.25rem] min-h-[3.25rem] btn-primary border-none"}
+              className={
+                "w-[15.5rem] btn rounded-full normal-case space-x-2 btn-lg h-[3.25rem] min-h-[3.25rem] btn-primary border-none"
+              }
               href="https://www.ousd.com/burn"
               type="external"
               newWindow
