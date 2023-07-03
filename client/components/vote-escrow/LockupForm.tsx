99760e8561913afbc6eff9648aaba8095679fd7c
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2023-07-03 19:52:04 +0530

    Fix APY calc

--- client/components/vote-escrow/LockupForm.tsx
+++ client/components/vote-escrow/LockupForm.tsx
@@ -145,16 +145,7 @@ const LockupForm: FunctionComponent<LockupFormProps> = ({ existingLockup }) => {
   // as specified here: https://github.com/OriginProtocol/ousd-governance/blob/master/contracts/OgvStaking.sol#L21
   const votingDecayFactor = 1.8;
 
-  // block.timestamp of when OgvStaking.sol was launched
-  const epoch = 1657584000;
-  const duration = lockupDuration * SECONDS_IN_A_MONTH;
-  // Since we'll be using blockTimestamp from CURRENT block, calculation will be
-  // a hair outdated... but it's negligible
-  const start = blockTimestamp > epoch ? blockTimestamp : epoch; // In prod, should always be blockTimestamp
-  const end = start + duration;
-  const dist = end - epoch; // Distance between end of staking period and the very beginning when staking was launched
-  const multiplier = dist / 365 / 86400;
-  const veOgvFromOgvLockup = lockupAmount * votingDecayFactor ** multiplier;
+  const veOgvFromOgvLockup = lockupAmount * votingDecayFactor ** (lockupDuration / 12);
 
   const ogvLockupRewardApy = getRewardsApy(
     veOgvFromOgvLockup,
