b4b5aa4f7bdd02b43a3f8848df4610bbe2dd65bc
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2024-05-26 22:01:13 +0530

    Deployment with Governance proposal (#425)
    * Deployment with Governance proposal
    
    * Fix launch date
    
    * Split proposal
    
    * Full proposal execution flow
    
    * Get rid of hardcoded timestamps
    
    * Update readme
    
    * Tweaks
    
    * Fix rewards fork test
    
    * Round up
    
    * Fix tests
    
    * Make proposal desc more detailed
    
    * Add deployment (#428)

--- script/deploy/mainnet/012_xOGNGovernanceScript.sol
+++ script/deploy/mainnet/012_xOGNGovernanceScript.sol
@@ -7,33 +7,25 @@ import {Vm} from "forge-std/Vm.sol";
 
 import {Addresses} from "contracts/utils/Addresses.sol";
 
-import {FixedRateRewardsSourceProxy} from "contracts/upgrades/FixedRateRewardsSourceProxy.sol";
-import {ExponentialStakingProxy} from "contracts/upgrades/ExponentialStakingProxy.sol";
-import {MigratorProxy} from "contracts/upgrades/MigratorProxy.sol";
-
-import {ExponentialStaking} from "contracts/ExponentialStaking.sol";
-import {FixedRateRewardsSource} from "contracts/FixedRateRewardsSource.sol";
-import {OgvStaking} from "contracts/OgvStaking.sol";
-import {Migrator} from "contracts/Migrator.sol";
 import {Timelock} from "contracts/Timelock.sol";
 import {Governance} from "contracts/Governance.sol";
 
 import {GovFive} from "contracts/utils/GovFive.sol";
 
-import {IMintableERC20} from "contracts/interfaces/IMintableERC20.sol";
+import {VmHelper} from "utils/VmHelper.sol";
 
 import "OpenZeppelin/openzeppelin-contracts@4.6.0/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "OpenZeppelin/openzeppelin-contracts@4.6.0/contracts/governance/TimelockController.sol";
 
 contract XOGNGovernanceScript is BaseMainnetScript {
     using GovFive for GovFive.GovFiveProposal;
+    using VmHelper for Vm;
 
-    GovFive.GovFiveProposal govFive;
+    GovFive.GovFiveProposal public govProposal;
 
     string public constant override DEPLOY_NAME = "012_xOGNGovernance";
 
-    uint256 constant OGN_EPOCH = 1717041600; // May 30, 2024 GMT
-    uint256 constant REWARDS_PER_SECOND = 300000 ether / uint256(24 * 60 * 60); // 300k per day
+    uint256 public constant OGN_EPOCH = 1717041600; // May 30, 2024 GMT
 
     constructor() {}
 
@@ -47,45 +39,57 @@ contract XOGNGovernanceScript is BaseMainnetScript {
 
         _recordDeploy("XOGN_GOV", address(governance));
 
-        _buildGnosisTx();
+        _buildGovernanceProposal();
     }
 
-    function _buildGnosisTx() internal {
+    function _buildGovernanceProposal() internal override {
         Timelock timelock = Timelock(payable(Addresses.TIMELOCK));
 
         address xognGov = deployedContracts["XOGN_GOV"];
 
-        govFive.setName("Enable OGN Governance & Begin Rewards");
+        address ognRewardsSourceProxy = deployedContracts["OGN_REWARDS_SOURCE"];
+        address veOgvImpl = deployedContracts["VEOGV_IMPL"];
 
-        govFive.setDescription("Grant roles on Timelock to OGN Governance");
+        govProposal.setName("Grant access to OGN Governance");
 
-        govFive.action(Addresses.TIMELOCK, "grantRole(bytes32,address)", abi.encode(timelock.PROPOSER_ROLE(), xognGov));
-        govFive.action(Addresses.TIMELOCK, "grantRole(bytes32,address)", abi.encode(timelock.CANCELLER_ROLE(), xognGov));
-        govFive.action(Addresses.TIMELOCK, "grantRole(bytes32,address)", abi.encode(timelock.EXECUTOR_ROLE(), xognGov));
+        govProposal.setDescription("Grant access to OGN Governance");
 
-        // Enable rewards for staking
-        govFive.action(
-            deployedContracts["OGN_REWARDS_SOURCE"],
-            "setRewardsPerSecond(uint192)",
-            abi.encode(uint192(REWARDS_PER_SECOND))
+        // Grant access to OGN Governance
+        govProposal.action(
+            Addresses.TIMELOCK, "grantRole(bytes32,address)", abi.encode(timelock.PROPOSER_ROLE(), xognGov)
+        );
+        govProposal.action(
+            Addresses.TIMELOCK, "grantRole(bytes32,address)", abi.encode(timelock.CANCELLER_ROLE(), xognGov)
+        );
+        govProposal.action(
+            Addresses.TIMELOCK, "grantRole(bytes32,address)", abi.encode(timelock.EXECUTOR_ROLE(), xognGov)
         );
 
-        if (!isForked) {
-            govFive.printTxData();
-        }
+        // Revoke access from Multisig
+        govProposal.action(
+            Addresses.TIMELOCK,
+            "revokeRole(bytes32,address)",
+            abi.encode(timelock.PROPOSER_ROLE(), Addresses.GOV_MULTISIG)
+        );
+        govProposal.action(
+            Addresses.TIMELOCK,
+            "revokeRole(bytes32,address)",
+            abi.encode(timelock.CANCELLER_ROLE(), Addresses.GOV_MULTISIG)
+        );
+        govProposal.action(
+            Addresses.TIMELOCK,
+            "revokeRole(bytes32,address)",
+            abi.encode(timelock.EXECUTOR_ROLE(), Addresses.GOV_MULTISIG)
+        );
     }
 
     function _fork() internal override {
-        IMintableERC20 ogn = IMintableERC20(Addresses.OGN);
+        // Simulate execute on fork by impersonating Timelock
+        govProposal.execute();
+    }
 
-        // Mint enough OGN to fund 100 days of rewards
-        vm.prank(Addresses.OGN_GOVERNOR);
-        ogn.mint(deployedContracts["OGN_REWARDS_SOURCE"], 30_000_000 ether);
-
-        // Go to the start of everything
-        vm.warp(OGN_EPOCH);
-
-        // Simulate execute on fork
-        govFive.execute();
+    function skip() external view override returns (bool) {
+        // Don't deploy on Mainnet for now
+        return !vm.isForkEnv();
     }
 }
