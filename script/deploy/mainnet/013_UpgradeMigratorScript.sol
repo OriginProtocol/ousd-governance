9a954fc492bc928af3c47c12b7726710596137e5
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2024-05-30 17:57:10 +0530

    Update deploy script

--- script/deploy/mainnet/013_UpgradeMigratorScript.sol
+++ script/deploy/mainnet/013_UpgradeMigratorScript.sol
@@ -15,6 +15,7 @@ import {GovFive} from "contracts/utils/GovFive.sol";
 import {VmHelper} from "utils/VmHelper.sol";
 
 import {Migrator} from "contracts/Migrator.sol";
+import {OgvStaking} from "contracts/OgvStaking.sol";
 
 import "OpenZeppelin/openzeppelin-contracts@4.6.0/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "OpenZeppelin/openzeppelin-contracts@4.6.0/contracts/governance/TimelockController.sol";
@@ -33,7 +34,15 @@ contract UpgradeMigratorScript is BaseMainnetScript {
         console.log("Deploy:");
         console.log("------------");
 
-        // Deploy implementation
+        // Deploy veOGV implementation
+        uint256 ogvMinStaking = 30 * 24 * 60 * 60; // 2592000 -> 30 days
+        uint256 ogvEpoch = OgvStaking(Addresses.VEOGV).epoch(); // Use old value.
+        OgvStaking veOgvImpl = new OgvStaking(
+            Addresses.OGV, ogvEpoch, ogvMinStaking, Addresses.OGV_REWARDS_PROXY, deployedContracts["MIGRATOR"]
+        );
+        _recordDeploy("VEOGV_IMPL", address(veOgvImpl));
+
+        // Deploy migrator implementation
         Migrator migratorImpl = new Migrator(Addresses.OGV, Addresses.OGN, Addresses.VEOGV, deployedContracts["XOGN"]);
         _recordDeploy("MIGRATOR_IMPL", address(migratorImpl));
     }
@@ -41,6 +50,10 @@ contract UpgradeMigratorScript is BaseMainnetScript {
     function _buildGovernanceProposal() internal override {
         govProposal.action(
             deployedContracts["MIGRATOR"], "upgradeTo(address)", abi.encode(deployedContracts["MIGRATOR_IMPL"])
+        );
+
+        govProposal.action(
+            deployedContracts["VEOGV"], "upgradeTo(address)", abi.encode(deployedContracts["VEOGV_IMPL"])
         );
     }
 
